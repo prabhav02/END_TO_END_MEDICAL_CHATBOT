3403f25b7b77fbd603c8980b9e4c86a7332bc30c
prabhav02 <febindian2004@gmail.com>
2025-09-05 16:05:21 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+  name="END_TO_END_MEDICAL_CHATBOT",
+  version="0.1.0",
+  author="Prabhav",
+  author_email="prabhav885@gmail.com",
+  packages=find_packages(),
+  install_requires=[]
+)
